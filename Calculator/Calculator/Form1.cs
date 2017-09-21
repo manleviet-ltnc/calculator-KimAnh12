404248180d2227f8e17c49ab7a0a5564dfe4ed2f
KimAnh12 <31847974+KimAnh12@users.noreply.github.com>
2017-09-21 20:51:19 +0700

    D1

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isTypingNumber = false;
+        private void NhapSo(object sender, EventArgs e)
+        {
+            string so = ((Button)sender).Text;
+            NhapSo(so);
+
+                }
+        private void NhapSo(string so)
+        {
+            if (isTypingNumber)
+                lblHienThi.Text = lblHienThi.Text + so ;
+            else
+            {
+                lblHienThi.Text = so ;
+                isTypingNumber = true;
+            }
+        }
+    }
+}
